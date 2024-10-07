bfde3ec4b28655f01719c67866a01555b5ec419c
ElmahdiAI <mehdirajawi239@gmail.com>
2024-10-08 00:31:36 +0100

    updated

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 import getListStudents from './0-get_list_students';
 
-export default function getStudentIdsSum (students = getListStudents()) {
-    return students.reduce((accumulator, student) => accumulator + student.id, 0);
+export default function getStudentIdsSum(students = getListStudents()) {
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
 }
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,16 @@
 export default function groceriesList() {
-    const groceries = new Map();
-    groceries.set('Apples', 10);
-    groceries.set('Tomatoes', 10);
-    groceries.set('Pasta', 1);
-    groceries.set('Rice', 1);
-    groceries.set('Banana', 5);
+  const arr = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
 
-    return groceries;
+  const mp = new Map();
+  // eslint-disable-next-line no-unused-vars
+  for (const el of arr) {
+    mp.set(el[0], el[1]);
+  }
+  return mp;
 }
